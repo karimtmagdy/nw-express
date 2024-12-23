83d00b0c40f1f410ba32dc76da47cd1c588cff25
karimtmagdy <karimtmagdy@gmail.com>
2024-12-23 17:55:22 +0200

    update

--- api/index.js
+++ api/index.js
@@ -8,17 +8,40 @@ import express from "express";
 // import express from 'express'
 // import express from 'express'
 // import express from 'express'
-
-ConfigApp(app);
+import cors from "cors";
+import session from "express-session";
+import morgan from "morgan";
+// ConfigApp(app);
 const app = express();
-const port = 8000 || process.env.PORT;
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://newave.vercel.app", "*"],
+    credentials: true,
+  })
+);
+app.use(
+    session({
+      secret: process.env.JWT_SECRET_KEY, // مفتاح الجلسة
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // كوكيز آمنة فقط في بيئة الإنتاج
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+      },
+    })
+  );
+ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.get("/favicon.ico", (req, res) => {
   res.status(204).send();
 });
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
