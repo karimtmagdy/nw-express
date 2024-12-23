532c9ea02a74da28b8c56022d17f9c6a1d2726c5
karimtmagdy <karimtmagdy@gmail.com>
2024-12-23 17:38:09 +0200

    update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 // import 'dotenv/config'
 import express from 'express'
+// import express from 'express'
+// import express from 'express'
+// import express from 'express'
+// import express from 'express'
+// import express from 'express'
+// import express from 'express'
+// import express from 'express'
 
 // const express = require('express')
 const app = express()
@@ -8,7 +15,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).send();
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
