40bfedba77bdda674a12666b77604d9e3dbb31e8
karimtmagdy <karimtmagdy@gmail.com>
2024-12-23 17:57:31 +0200

    update

--- api/config/config.js
+++ api/config/config.js
@@ -3,11 +3,12 @@ import cors from "cors";
 import session from "express-session";
 import morgan from "morgan";
 export const ConfigApp = (app) => {
+  const app = express();
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
-      origin: ["http://localhost:3000", "https://newave.vercel.app"],
+      origin: ["http://localhost:3000", "https://newave.vercel.app", "*"],
       credentials: true,
     })
   );
--- api/index.js
+++ api/index.js
@@ -11,30 +11,30 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import morgan from "morgan";
-// ConfigApp(app);
 const app = express();
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://newave.vercel.app", "*"],
-    credentials: true,
-  })
-);
-app.use(
-    session({
-      secret: process.env.JWT_SECRET_KEY, // مفتاح الجلسة
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // كوكيز آمنة فقط في بيئة الإنتاج
-        sameSite: "strict",
-        maxAge: 1000 * 60 * 60 * 24 * 30,
-      },
-    })
-  );
- if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+ConfigApp(app);
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://newave.vercel.app", "*"],
+//     credentials: true,
+//   })
+// );
+// app.use(
+//     session({
+//       secret: process.env.JWT_SECRET_KEY, // مفتاح الجلسة
+//       resave: false,
+//       saveUninitialized: false,
+//       cookie: {
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === "production", // كوكيز آمنة فقط في بيئة الإنتاج
+//         sameSite: "strict",
+//         maxAge: 1000 * 60 * 60 * 24 * 30,
+//       },
+//     })
+//   );
+//  if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
