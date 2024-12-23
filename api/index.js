f32d1c7abfd2c7e383f42059b8382200874676f5
karimtmagdy <karimtmagdy@gmail.com>
2024-12-23 20:57:09 +0200

    update cors

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://newave.vercel.app", "*"],
+    origin: ["http://localhost:3000", "https://newave-store.vercel.app", "*"],
     credentials: true,
   })
 );
