12d9310dace5d620d3487910acae35dcf514fb7b
karimtmagdy <karimtmagdy@gmail.com>
2024-12-23 18:23:21 +0200

    update mongo

--- api/config/db.js
+++ api/config/db.js
@@ -1,12 +1,23 @@
+// process.env.DB_VERCEL.replace("<PASSWORD>", process.env.DB_PASSWORD)
 import mongoose from "mongoose";
 const Message = "Message : API is connected Successfully";
-export const database =  () => {
-  mongoose.connect(process.env.MONGO_URI.toString());
-  // process.env.DB_VERCEL.replace("<PASSWORD>", process.env.DB_PASSWORD)
-  mongoose.connection.once("open", () => {
-    console.log(Message);
-  });
+
+export const database =()=>{
+  mongoose.connect(process.env.MONGO_URI);
+  console.log(Message);
+  // mongoose.connection.once("open", () => {
+  // });
   mongoose.connection.on("error", (error) => {
     console.error("Error connecting to MongoDB:", error);
   });
-};
+}
+
+// export const database =  () => {
+//   mongoose.connect(process.env.MONGO_URI.toString());
+//   mongoose.connection.once("open", () => {
+//     console.log(Message);
+//   });
+//   mongoose.connection.on("error", (error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+// };
