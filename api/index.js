ecd63f6de239aa4fa22a5ca3eabde350fbe88760
karimtmagdy <karimtmagdy@gmail.com>
2025-02-27 12:56:01 +0200

    update server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,42 @@
 import "dotenv/config";
 import express from "express";
-import { startServerApplication } from "./server.js";
+import { development, port } from "./constants/env.js";
+// import { startServerApplication } from "./server.js";
 const app = express();
 app.use(express.json());
-startServerApplication(app);
+ const server = app
+    .listen(port, () => {
+      console.log(
+        `Server started in ${
+          development ? "development" : "production"
+        } mode on port ${port}`
+      );
+    })
+    .on("error", (err) => {
+      if (err.code === "EADDRINUSE") {
+        console.error(`error: Port ${port} is already in use.`);
+        process.exit(1);
+      }
+    });
+  // التعامل مع الأخطاء غير المعالجة
+  process.on("unhandledRejection", (err) => {
+    console.error(`Unhandled Rejection: (${err.name} - ${err.message})`);
+    server.close(() => {
+      console.error("Shutting down server...");
+      process.exit(1);
+    });
+  });
+  // التعامل مع إيقاف التشغيل اليدوي
+  process.on("SIGINT", async () => {
+    console.log("Received SIGINT, shutting down gracefully...");
+    try {
+      database;
+      console.log("Database connection closed.");
+    } catch (error) {
+      console.error("Error closing database connection:", error);
+    }
+    server.close(() => {
+      console.log("Server closed.");
+      process.exit(0);
+    });
+  });
