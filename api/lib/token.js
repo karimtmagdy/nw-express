0c7749ca78ca4dea07453a513e7fbf6396c2f70f
karimtmagdy <karimtmagdy@gmail.com>
2025-02-27 09:24:18 +0200

    update

--- api/lib/token.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-export const signToken = (payload, param, expire) => {
-  return jwt.sign({ ...payload }, process.env[param], { expiresIn: expire });
-};
-export const verifyToken = (token, param) => {
-  return jwt.verify(token, process.env[param]);
-};
--- api/middlewares/validate.js
+++
@@ -1,9 +0,0 @@
-export const validate = (schema) => (req, res, next) => {
-  try {
-    schema.parse(req.body);
-    next();
-  } catch (err) {
-    return res.status(400).json({ errors: err });
-  }
-};
-// .errors.map((e) => e.message)
