b4194fea25424ddd0d3753acecd816c27fd06163
karimtmagdy <karimtmagdy@gmail.com>
2025-03-06 01:12:17 +0200

    update route subcategories

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 import { authRoutes } from "./auth.routes.js";
 import { usersRoutes } from "./user.routes.js";
 import { categoriesRoutes } from "./category.routes.js";
-// import { subCategoryRoutes } from "./subcategory.routes.js";
+import { subcategoriesRoutes } from "./subcategory.routes.js";
 // import { uploadRoutes } from "./upload.routes.js";
 
 const api = "/api/v1";
@@ -9,17 +9,13 @@ export const RouterApiApplication = (app) => {
   app.use(`${api}/auth`, authRoutes);
   app.use(`${api}/users`, usersRoutes);
   app.use(`${api}/categories`, categoriesRoutes);
-  // app.use(`${api}/subcategories`, subCategoryRoutes);
-
-  //   app.use(`${api}/products`, productRoutes);
+  app.use(`${api}/subcategories`, subcategoriesRoutes);
   //   app.use(`${api}/brands`, brandRoutes);
-  //   app.use(`${api}/auth`,);
+  //   app.use(`${api}/products`, productRoutes);
+  //   app.use(`${api}/orders`,orderRoutes);
 };
 
 // .use("/api/v1/profile", profileRoutes);
 // .use("/api/v1/account", accountRoutes);
-
 // .use("/api/v1/upload", uploadRoutes);
 // .use('/api/v1/profile',profileRoutes)
-// .use('/api/v1/orders',orderRoutes)
-// .use('/api/v1/admin',adminRoutes)
--- api/routes/subcategory.routes.js
+++ api/routes/subcategory.routes.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validate.js";
 import { isAdmin } from "../middlewares/JWTauth.js";
+import {
+  createSubCategory,
+  deleteSubCategory,
+  getSubCategories,
+  singleSubCategory,
+  updateSubCategory,
+} from "../services/subcategory.service.js";
 const router = Router();
-router.route("/").post(isAdmin, validate("body")).get();
+router
+  .route("/")
+  .post(isAdmin, validate("body"), createSubCategory)
+  .get(getSubCategories);
 router
   .route("/:id")
-  .get()
-  .patch(isAdmin, validate("body"))
-  .delete(isAdmin, validate("params"));
+  .get(singleSubCategory)
+  .patch(isAdmin, validate("body"), updateSubCategory)
+  .delete(isAdmin, validate("params"), deleteSubCategory);
 export { router as subcategoriesRoutes };
