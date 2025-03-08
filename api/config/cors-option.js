640aee646bc35828f4094616271fb112b286e69e
karimtmagdy <karimtmagdy@gmail.com>
2025-03-08 02:15:35 +0200

    update cors

--- api/config/cors-option.js
+++ api/config/cors-option.js
@@ -1,4 +1,6 @@
-// import { client, frontend, frontend_local } from "../constants/env.js";
+//origin: "http://localhost:5173", // السماح بطلبات من هذا الدومين فقط
+
+import { client, frontend, frontend_local } from "../constants/env.js";
 
 const allowedOrigins = [
   "http://localhost:3000",
@@ -6,20 +8,17 @@ const allowedOrigins = [
   "https://newave-store.vercel.app",
 ];
 // const allowedOrigins = [client, frontend, frontend_local];
+
 export const corsOption = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true, // السماح بإرسال الكوكيز
-  methods: ["GET POST PUT PATCH DELETE"], // السماح بهذه الطلبات فقط
-  allowedHeaders: ["Content-Type", "Authorization"], // السماح بهذه الهيدرات فقط
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // السماح بهذه الطلبات فقط
+  allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"], // السماح بهذه الهيدرات
+  optionsSuccessStatus: 200, // تجنب أخطاء في المتصفحات القديمة
 };
-
-//origin: "http://localhost:5173", // السماح بطلبات من هذا الدومين فقط
-   
-    
-   
