ac2fac7a7555fa49f9c3119b75c1ffbb971e2dc4
karimtmagdy <karimtmagdy@gmail.com>
2025-03-23 14:32:06 +0200

    update cors

--- api/index.js
+++ api/index.js
@@ -16,7 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+// app.use(cors({ credentials: true, origin:'http://localhost:3000' }));
+app.use(cors({ credentials: true, ...corsOptions }));
 app.use(
   helmet({
     crossOriginResourcePolicy: false,
