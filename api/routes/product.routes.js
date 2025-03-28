9ffdf3fa20712391754e92a19d403c4f79353424
karimtmagdy <karimtmagdy@gmail.com>
2025-03-28 05:00:55 +0200

    create products routes

--- api/routes/product.routes.js
+++ api/routes/product.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { isAdmin } from "../middlewares/JWTauth.js";
+import { validID } from "../middlewares/validID.js";
+import { deleteProduct, getProducts, singleProduct, updateProduct } from "../services/product.service.js";
 
 const router = Router();
-router.route('/').post().get();
-router.route('/:id').get().patch().delete();
+router.route('/').post(isAdmin,    createCategory).get(getProducts);
+router.route('/:id').get(validID, singleProduct).patch(isAdmin , updateProduct).delete(isAdmin, validID, deleteProduct);
 export { router as productsRoutes };
